230ff91bfe05ad7cc7854798cbef002577f84cd2
shanghaiellen <shanghaiellen@gmail.com>
2014-08-26 14:22:47 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Ada Alumnae Association
+=======================
+
+This is a repo for the Ada Alumnae Association website. It doesn't have a whole lot going for it right now, but we'll change that in time!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AAA::Application.config.secret_key_base = '8f6a7ce990a4a4172d74b09251f164bd33e2bef56831c55759f62df97605e9ceb4606839c76e8f1cffcd1a6f75221efa878fea53fc73117b15a1849a1c404697'
